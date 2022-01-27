f638008cc6bfadeabdaf8175b6ab01e5b04816b0
Jan <jan.kipka@gmx.de>
2022-01-27 21:18:47 +0100

    Initial Commit

---
+++ BookPal/BookPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookPalApp.swift
+//  BookPal
+//
+//  Created by Jan Kipka on 27.01.22.
+//
+
+import SwiftUI
+
+@main
+struct BookPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookPal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookPal
+//
+//  Created by Jan Kipka on 27.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
